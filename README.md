cb8d877da9a4e0964c0ba05ef0c6671960cc3804
Debopam Sengupta <dev1994@connect.hku.hk>
2015-12-15 03:11:55 +0800

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # node-sheetsu
-Node JS SDK for using Sheetsu
+Node JS module for using Sheetsu
+
+## Getting Started
+* Install the module ```npm install sheetsu```
+* Example usage :
+```
+var Sheetsu = require('sheetsu');
+
+var test_url = 'YOUR_SHEETSU_API';
+
+var dev = new Sheetsu(test_url);
+
+dev.getData({column_name:'...'}, function(response) {
+    console.log(response.data);
+});
+dev.addRow({...}, function(response) {
+    console.log(response);
+})
+```
+
+## Methods
+
+### getAllData (callback)
+* ```callback``` : function to be executed after the method
+
+### getData (params, callback)
+* ```params``` : Should include the field ```column_name``` to get data of the particular column
+* ```callback``` : function to be executed after the method
+
+### addRow (params, callback)
+* ```params``` : Should include the row of information to add to the spreadsheet
+* ```callback``` : function to be executed after the method
---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict';
+const request = require('request');
+
+class Sheetsu {
+    constructor(url) {
+        this.url = url;
+    }
+
+    getAllData(callback) {
+        let get_url = this.url;
+        let request_params = {
+            url : get_url,
+            method: 'GET'
+        };
+        request(request_params, function(err, response, body) {
+            if(err) {
+                throw(err);
+            }
+            body = JSON.parse(body);
+            if(body.status === 200 && body.success === true) {
+                let results = body.result;
+                callback({meta:{code: 200}, data: results});
+            }
+        });
+    }
+
+    getData(params, callback) {
+        let get_url = this.url + '/column/:column_name';
+        let request_params = {
+            url : get_url.replace(':column_name', params.column_name),
+            method: 'GET'
+        };
+        request(request_params, function(err, response, body) {
+            if(err) {
+                throw(err);
+            }
+            body = JSON.parse(body);
+            if(body.status === 200 && body.success === true) {
+                let results = body.result;
+                callback({meta:{code: 200}, data: results});
+            }
+        });
+    }
+
+    addRow(params, callback) {
+        let request_params = {
+            url : this.url,
+            method: 'POST',
+            json: params,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+        request(request_params, function(err,response,body){
+            if(err) {
+                throw(err);
+            }
+            if(body.status === 201 && body.success === true) {
+                callback({meta: {code:201} , data:[]});
+            }
+        });
+    }
+}
+
+module.exports = Sheetsu;
